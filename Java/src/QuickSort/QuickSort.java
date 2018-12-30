92a7d4569f7e490b61e20e5e18e45bf86390124a
Dulanga Heshan <164056m@uom.lk>
2018-12-30 15:02:54 +0530

    quick sort(selecting pivot at end)

--- Java/src/QuickSort/QuickSort.java
+++ Java/src/QuickSort/QuickSort.java
@@ -1,8 +1,8 @@
 package QuickSort;
 
-class QuickSort {
+public class QuickSort {
 
-    int partition(int arr[],int low,int high){
+    public int partition(int arr[],int low,int high){
 
         int pivot = arr[high];
 
@@ -22,7 +22,7 @@ class QuickSort {
         return i+1;
 }
 
-void sort(int arr[],int low,int high){
+public void sort(int arr[],int low,int high){
 
         if(low < high){
 
@@ -33,7 +33,7 @@ void sort(int arr[],int low,int high){
 }
 
 
-    static void printArray(int arr[])
+   public void printArray(int arr[])
     {
         int n = arr.length;
         for (int i=0; i<n; ++i)
@@ -41,7 +41,7 @@ void sort(int arr[],int low,int high){
 
     }
 
-    void swap(int arr[],int i ,int j){
+    public void swap(int arr[],int i ,int j){
 
         int temp = arr[i];
         arr[i] = arr[j];
@@ -54,10 +54,10 @@ void sort(int arr[],int low,int high){
         int arr[] = {10, 80, 30, 90, 40, 50, 70};
         int n = arr.length;
 
-        QuickSort ob = new QuickSort();
-        ob.sort(arr, 0, n-1);
+        QuickSort qsrt = new QuickSort();
+        qsrt.sort(arr, 0, n-1);
 
-        printArray(arr);
+        qsrt.printArray(arr);
     }
 
 
