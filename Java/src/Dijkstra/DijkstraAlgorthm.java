e0efbf520f4c308fdc0a2019041b573ade6dd6bb
dulangaheshan <dulangah2@gmail.com>
2019-05-15 01:18:24 +0530

    algorthm implemented using priority que

---
+++ Java/src/Dijkstra/DijkstraAlgorthm.java
@@ -0,0 +1,59 @@
+package Dijkstra;
+
+import java.util.*;
+
+public class DijkstraAlgorthm{
+
+    public void computePath(Vertex sourceVertex){
+
+        sourceVertex.setDistance(0);
+        PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.add(sourceVertex);
+
+        while( !priorityQueue.isEmpty()){
+
+            Vertex actualVertex = priorityQueue.poll();
+
+            for(Edge edge : actualVertex.getAdjacenciesList()){
+
+                Vertex v = edge.getTargetVertex();
+
+                double newDistance = actualVertex.getDistance() + edge.getWeight();
+
+                if(newDistance < v.getDistance()){
+                    priorityQueue.remove(v);
+                    v.setDistance(newDistance);
+                    v.setPredecessor(actualVertex);
+                    priorityQueue.add(v);
+
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+    public List<Vertex> getShortestPathTo(Vertex targetVertex){
+
+        List<Vertex> shortestPathToTarget = new ArrayList<>();
+
+        for(Vertex vertex = targetVertex;vertex != null;vertex = vertex.getPredecessor()){
+
+            shortestPathToTarget.add(vertex);
+        }
+
+        Collections.reverse(shortestPathToTarget);
+
+        return shortestPathToTarget;
+
+    }
+
+
+
+
+
+}
